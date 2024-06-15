fc91b206ff7e86f17e5e1a6cea0894b8900839c1
Doğuhan İlter <doguhannilt@gmail.com>
2024-06-16 02:16:10 +0300

    Navigation CSS added & isAdmin added

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -43,36 +43,104 @@ const Navigation = () => {
     }
 
     return (
-        <div style={{ zIndex: 999 }} className={`${showSidebar ? "hidden" : 'flex'} xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-black w-[4%] h-[100vh] fixed`}>
+        <div style={{ zIndex: 999 }} className={`${showSidebar ? "hidden" : 'flex'} xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[6%] hover:w-[12%] h-[100vh]  fixed `}>
             
             <div className="flex flex-col justify-center space-y-4">
                 <Link to='/' className='flex items-center transition-transform tansform hover:translate-x-2'>
                     <AiOutlineHome className='mr-2 mt-[3rem]' size={26} />
+                    <span className='mt-12'>Home</span>
                 </Link>
                 <Link to='/shop' className='flex items-center transition-transform tansform hover:translate-x-2'>
                     <AiOutlineShopping className='mr-2 mt-[3rem]' size={26} />
+                    <span className='mt-12'>Shop</span>
                 </Link>
                 <Link to='/cart' className='flex items-center transition-transform tansform hover:translate-x-2'>
                     <AiOutlineShoppingCart className='mr-2 mt-[3rem] text-white' size={26} />
+                    <span className='mt-12'>Cart</span>
                 </Link>
                 <Link to='/favorite' className='flex items-center transition-transform tansform hover:translate-x-2 '>
                     <FaHeart className='mr-2 mt-[3rem]' size={26} />
+                <span className='mt-12'>Favs</span>
                 </Link>
             </div>
 
             <div className="relative">
                 <button onClick={toggleDropdown} className='flex items-center text-gray-8000 focus:outline-none'> 
                     {userInfo ? (
-                        <span className='text-white'>{userInfo.username}</span>
+                        <span className='text-white font-bold text-xl'>{userInfo.username}</span>
                     ) : (
                           <></>  
                     )}    
-                
-                </button>
-            </div>
 
-        <ul>
+                                        
+                {userInfo && (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={`h-4 w-4 ml-1 ${
+                            dropdownOpen ? "transform rotate-180" : ""
+                            }`}
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='white'
+                        >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth="2"
+                            d = {dropdownOpen ? "M5 15l7-7 7 7": "M19 9Ş-7 7-7-7"}
+                        />
+                    </svg>
+                    )}
+                </button>
+                {dropdownOpen && userInfo && (
+    <ul className={`absolute right-0 mt-2 mr-14 space-y-2 bg-white text-gray-600 ${!userInfo.isAdmin ? '-top-20' : '-top-80'}`}>
+        {userInfo.isAdmin ? (
+            <>
                 <li>
+                    <Link to='/admin/dashboard' className='block px-4 py-2 hover:bg-gray-200'>Dashboard</Link>
+                </li>
+                <li>
+                    <Link to='/admin/productlist' className='block px-4 py-2 hover:bg-gray-200'>Products</Link>
+                </li>
+                <li>
+                    <Link to='/admin/categorylist' className='block px-4 py-2 hover:bg-gray-200'>Category L.</Link>
+                </li>
+                <li>
+                    <Link to='/admin/orderlist' className='block px-4 py-2 hover:bg-gray-200'>Order L.</Link>
+                </li>
+                <li>
+                    <Link to='/admin/userlist' className='block px-4 py-2 hover:bg-gray-200'>Users</Link>
+                </li>
+                <li>
+                    <Link to='/admin/profile' className='block px-4 py-2 hover:bg-gray-200'>Profile</Link>
+                </li>
+                <li>
+                    <Link to='/admin/logout'
+                        onClick={logoutHandler}
+                        className='block px-4 ml-10 hover:bg-gray-200'>Logout</Link>
+                </li>
+            </>
+        ) : (
+            <>
+                <li>
+                    <Link to='/admin/profile' className='block px-4 py-2 hover:bg-gray-200'>Profile</Link>
+                </li>
+                <li>
+                    <Link to='/'
+                        onClick={logoutHandler}
+                        className='block px-4 py-2 hover:bg-gray-200'>Logout</Link>
+                </li>
+            </>
+        )}
+    </ul>
+)}
+
+
+            </div>
+            {!userInfo && (
+                
+            <ul>
+             <li>
                 <Link to='/login' className='flex items-center transition-transform tansform hover:translate-x-2'>
                     <AiOutlineLogin className='mr-2 mt-[3rem]' size={26} />
              
@@ -84,7 +152,9 @@ const Navigation = () => {
                 
                 </Link>
                 </li> 
-        </ul>
+            </ul>
+                )}
+
         
         </div>
   )
