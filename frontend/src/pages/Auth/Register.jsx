30a0fb908c3a6e96a468e46a928f2f063f98ab08
Doğuhan İlter <doguhannilt@gmail.com>
2024-06-17 17:56:02 +0300

    Register page template added

---
+++ frontend/src/pages/Auth/Register.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react"
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+
+// REDUX
+import { useDispatch, useSelector } from "react-redux"
+import { setCredentials } from "../../redux/features/auth/authSlice"
+import { useRegisterMutation } from "../../redux/api/usersApiSlice"
+
+// TOAST
+import { toast } from "react-toastify"
+
+const Register = () => {
+  return (
+
+      <div>
+          
+      </div>
+
+    )
+}
+
+export default Register
