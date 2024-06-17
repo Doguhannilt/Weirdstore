cf1477c6108fa6e00afbf6f7273b7ac4709569a4
Doğuhan İlter <doguhannilt@gmail.com>
2024-06-17 18:46:49 +0300

    Register form created

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -10,11 +10,104 @@ import { useRegisterMutation } from "../../redux/api/usersApiSlice"
 import { toast } from "react-toastify"
 
 const Register = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const [register, {isLoading}] = useRegisterMutation()
+    const { userInfo } = useSelector(state => state.auth)
+    const { search } = useLocation()
+    const sp = new URLSearchParams(search)
+    const redirect = sp.get('redirect') || '/'
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect)
+        }
+    }, [navigate, redirect, userInfo ])
+
   return (
 
-      <div>
+      <section className="pl-[10rem]">
           
-      </div>
+          <div className="mr-[4rem] mt-[5rem]">
+              <h1 className="text-4xl font-semibold mb-4">Register</h1>
+
+              <form className="container w-[40rem]">
+                <div className="my-[2rem]">
+                    <label htmlFor="name" className="block text-sm font-medium text-white">Name</label>      
+                      <input
+                          type="text"
+                          id="name"
+                          className="mt-1 p-2 border rounded w-full"
+                          placeholder="Enter name"
+                          value={username}
+                          onChange={e => setUsername(e.target.value)}
+                          autoFocus />
+                </div>
+                <div className="my-[2rem]">
+                    <label htmlFor="email" className="block text-sm font-medium text-white">Email Address</label>      
+                      <input
+                          type="email"
+                          id="email"
+                          className="mt-1 p-2 border rounded w-full"
+                          placeholder="test@gmail.com"
+                          value={email}
+                          onChange={e => setEmail(e.target.value)}
+                           />
+                </div>
+                <div className="my-[2rem]">
+                    <label htmlFor="password" className="block text-sm font-medium text-white">Password</label>      
+                      <input
+                          type="password"
+                          id="password"
+                          className="mt-1 p-2 border rounded w-full"
+                          placeholder="Enter name"
+                          value={password}
+                          onChange={e => setPassword(e.target.value)}
+                           />
+                </div>
+                <div className="my-[2rem]">
+                    <label htmlFor="confirmPassword" className="block text-sm font-medium text-white">Confirm Password</label>      
+                      <input
+                          type="password"
+                          id="confirmPassword"
+                          className="mt-1 p-2 border rounded w-full"
+                          placeholder="Confirm Password"
+                          value={confirmPassword}
+                          onChange={e => setConfirmPassword(e.target.value)}
+                           />
+                  </div>
+                  
+                  <button
+                      className="bg-pink-500 text-white px-4 py-2 rounded cursor-pointer my-[1rem]"
+                      disabled={isLoading}
+                      type="submit">
+                      {isLoading ? "Registering..." : "Register"}
+                  </button>
+
+                  <div className="mt-4">
+                      <p className="text-white">
+                          Already have an Account? {" "}
+                          <Link
+                              to={redirect ? `/login?redirect=${redirect}` : "/login"}
+                              className="text-pink-700 hover:underline" 
+                          >
+                          Login
+                          </Link>
+                      </p>
+                  </div>
+              </form>    
+    
+          </div>
+
+      </section>
+
+
 
     )
 }
