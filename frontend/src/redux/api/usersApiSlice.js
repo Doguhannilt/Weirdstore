8e5d227ebb00b45ff219d80d3a07895545ee7dc8
Doğuhan İlter <doguhannilt@gmail.com>
2024-06-18 19:46:55 +0300

    useGetUsersQuery,     useDeleteUserMutation,     useGetUserDetailsQuery,     useUpdateUserMutation added

--- frontend/src/redux/api/usersApiSlice.js
+++ frontend/src/redux/api/usersApiSlice.js
@@ -33,13 +33,44 @@ export const userApiSlice = apiSlice.injectEndpoints({
                 method: 'PUT',
                 body: data,
             })
-        })
-    })
-})
-
+        }),
+        getUsers: builder.query({
+            query: () => ({
+                url: USERS_URL,
+            }),
+            providesTags: ['User'],
+            keepUnusedDataFor: 5,
+        }),
+        deleteUser: builder.mutation({
+            query: (userId) => ({
+                url: `${USERS_URL}/${userId}`,
+                method: 'DELETE',
+            }),
+        }),
+        getUserDetails: builder.query({
+            query: (id) => ({
+                url: `${USERS_URL}/${id}`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        updateUser: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/${data.userId}`,
+                method: 'PUT',
+                body: data,
+            }),
+            invalidatesTags: ['User'],
+        }),
+    }),
+});
 
 export const {
     useLoginMutation,
     useLogoutMutation,
     useRegisterMutation,
-    useProfileMutation} = userApiSlice
+    useProfileMutation,
+    useGetUsersQuery,
+    useDeleteUserMutation,
+    useGetUserDetailsQuery,
+    useUpdateUserMutation,
+} = userApiSlice;
