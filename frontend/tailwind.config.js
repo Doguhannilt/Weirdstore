a6f9177a9ac444e6cf599700f39bc6fb134f72dd
Doğuhan İlter <doguhannilt@gmail.com>
2024-06-19 02:59:03 +0300

    Flowbite plugin added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,6 @@
+import flowbitePlugin from 'flowbite/plugin'
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,6 +10,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbitePlugin
+  ],
 }
 
