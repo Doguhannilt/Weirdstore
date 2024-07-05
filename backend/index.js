ff88e123b102eb7189b1dc306e3f9ded849729a2
Doğuhan İlter <doguhannilt@gmail.com>
2024-07-05 05:50:37 +0300

    addImage added & Postman checked

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ import connectiontoDB from './config/database.js'
 import userRoutes from './routes/userRoutes.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 const port = process.env.PORT || 5000
@@ -27,5 +28,9 @@ app.use(cookieParser())
 app.use('/api/users', userRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/products', productRoutes)
+app.use('/api/upload', uploadRoutes)
 
-app.listen(port, () => console.log(`Server running on port: ${port}`))
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
+
+app.listen(port, () => console.log(`Server running on port: ${port}`)) 
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -19,7 +19,7 @@ const productSchema = mongoose.Schema({
     image: { type: String, required: true },
     brand: { type: String, required: true },
     quantity: { type: Number, required: true },
-    category: { type: ObjectId, ref: 'Category', required: true },
+    category: {type: ObjectId, ref: 'Category', required: true },
     description: { type: String, required: true },
     reviews: [reviewSchema],
     rating: { type: Number, required: true, default: 0 },
