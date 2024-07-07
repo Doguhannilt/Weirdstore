12b6f5da1e6cbb8b4c84882d993aeeeb423ad4fe
Doğuhan İlter <doguhannilt@gmail.com>
2024-07-07 04:09:14 +0300

    productApiSlice added

---
+++ frontend/src/redux/api/productApiSlice.js
@@ -0,0 +1,93 @@
+import { PRODUCT_URL, UPLOAD_URL } from "../constants";
+import { apiSlice } from './apiSlice'
+
+export const productApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: ({ keyword }) => ({
+                url: `${PRODUCT_URL}`,
+                params: {keyword},
+            }),
+
+            keepUnusedDataFor: 5,
+            providesTags: ['Product']
+        }),
+
+        getProductById: builder.query({
+            query: (productId) => `${PRODUCT_URL}/${productId}`,
+            providesTags: (result, error, productId) => [
+                {type: 'Product', id: productId}
+            ]
+        }),
+
+        allProducts: builder.query({
+            query: () => `${PRODUCT_URL}/allProducts`
+        }),
+
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url:`${PRODUCT_URL}/${productId}`
+            }),
+            keepUnusedDataFor: 5,
+        }),
+
+        createProduct: builder.mutation({
+            query: (productData) => ({
+                url: `${PRODUCT_URL}`,
+                method: 'POST',
+                body: productData,
+            }),
+
+            invalidatesTags: ['Product'],
+        }),
+
+        updateProduct: builder.mutation({
+            query: ({ productId, formData }) => ({
+                url: `${PRODUCT_URL}/${productId}`,
+                method: 'PUT',
+                body: formData
+            }),
+        }),
+
+        deleteProduct: builder.mutation({
+            query: (productId) => ({
+                url: `${PRODUCT_URL}/${productId}`,
+                method: 'DELETE',
+                body: productId
+            }),
+            providesTags: ['Product']
+        }),
+
+        createReview: builder.mutation({
+            query: (data) => ({
+                url: `${PRODUCT_URL}/${data.productId}/reviews`,
+                method: 'POST',
+                body: data
+            }),
+        }),
+
+        getTopProducts: builder.query({
+            query: () => `${PRODUCT_URL}/top`,
+            keepUnusedDataFor: 5,
+        }),
+
+        getNewProducts: builder.query({
+            query: () => `${PRODUCT_URL}/new`,
+            keepUnusedDataFor: 5
+        })
+    })
+})
+
+
+export const {
+    useGetProductsQuery,
+    useGetProductByIdQuery,
+    useAllProductsQuery,
+    useGetProductDetailsQuery,
+    useCreateProductMutation,
+    useUpdateProductMutation,
+    useDeleteProductMutation,
+    useCreateReviewMutation,
+    useGetTopProductsQuery,
+    useGetNewProductsQuery
+} = productApiSlice
