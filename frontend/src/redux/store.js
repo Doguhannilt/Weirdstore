b8c63d1a2fc2d88ee2ff1ca565cf71972cf46837
Doğuhan İlter <doguhannilt@gmail.com>
2024-07-08 20:33:36 +0300

    store.js updated for favorites

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from './features/auth/authSlice'
+import favoritesReducer from '../redux/features/favorites/favoritesSlice'
+import { getFavoritesFromLocalStorage } from "../utils/localStorage";
+
+const initalFavorites = getFavoritesFromLocalStorage() || []
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authReducer,
+        favorites: favoritesReducer,
+    },
+
+    preloadedState: {
+        favorites: initalFavorites
     },
 
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
