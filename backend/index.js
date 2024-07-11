8a610f64785463b48b7a43e4b8f081ebc1f8dc7d
Doğuhan İlter <doguhannilt@gmail.com>
2024-07-11 03:44:50 +0300

    api/orders endpoint added

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ import userRoutes from './routes/userRoutes.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config()
 const port = process.env.PORT || 5000
@@ -29,6 +30,7 @@ app.use('/api/users', userRoutes)
 app.use('/api/category', categoryRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/upload', uploadRoutes)
+app.use('/api/orders', orderRoutes)
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
