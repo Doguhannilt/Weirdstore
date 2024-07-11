7539164be6b47c08391dce24287a2a0db53429df
Doğuhan İlter <doguhannilt@gmail.com>
2024-07-11 04:23:17 +0300

    getAllOrders component added

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -95,3 +95,5 @@ const orderSchema = mongoose.Schema({
 
 const Order = mongoose.model('Order', orderSchema)
 export default Order
+
+
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,13 +4,17 @@ import { authenticate, authorizeAdmin } from '../middlewares/authMidd.js'
 
 
 // orderController.js
-import { createOrder } from "../controller/orderController.js";
+import {
+    createOrder,
+    getAllOrders
+} from "../controller/orderController.js";
 
 const router = express.Router()
 
 router
     .route('/')
     .post(authenticate, createOrder)
+    .get(authenticate, authorizeAdmin, getAllOrders)
 
 
 
