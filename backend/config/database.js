bbdefa59a3e9dcc0def07fc18ad477877ed6230a
Doğuhan İlter <doguhannilt@gmail.com>
2024-07-25 21:50:25 +0300

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,7 +7,7 @@ dotenv.config();
 const connectDB = async () => {
     try {
         // .env dosyasından MONGO_URI'yi al
-        const mongoURI = "mongodb://0.0.0.0:27017/"
+        const mongoURI = "mongodb://mongo:27017/dogu"
 
         if (!mongoURI) {
             console.error('MONGO_URI environment variable is not set');
