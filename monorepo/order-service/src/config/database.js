6ee19da467ffade56da90a27f42f7fe2a8c14f28
doguhannilt <doguhannilt@gmail.com>
2024-12-23 18:34:33 +0300

    order-service updated

---
+++ monorepo/order-service/src/config/database.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// .env dosyasını yükle
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const MONGO_URI = process.env.MONGO_URI
+
+        if (!MONGO_URI) {
+            console.error('MONGO_URI environment variable is not set');
+            process.exit(1);
+        }
+
+        await mongoose.connect(MONGO_URI);
+        console.log(`Successfully connected to the database`);
+    } catch (err) {
+        console.error("Database Error", err);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
