0eba89ecba2f867bc01fdcfb2efc7701ee3dad2e
doguhannilt <doguhannilt@gmail.com>
2024-12-23 18:34:17 +0300

    category-service updated

---
+++ monorepo/category-service/src/routes/categoryRoutes.js
@@ -0,0 +1,55 @@
+import express from 'express'
+const router = express.Router()
+import {authenticate, authorizeAdmin} from '../utils/authMidd.js'
+import {
+    createCategory,
+    DeleteById,
+    listCategory,
+    getAllCategory,
+    getCategoryById,
+    updateCategory
+} from '../controller/categoryController.js'
+
+
+router
+    .route('/')
+    // Create a category http://localhost:5000/api/category/
+    .post(
+        authenticate,
+        authorizeAdmin,
+        createCategory)
+    // Get all categories http://localhost:5000/api/category/
+    .get(
+        authenticate,
+        authorizeAdmin,
+        getAllCategory
+    )
+
+router
+    .route('/:categoryId')
+    // Update a category http://localhost:5000/api/category/:categoryId
+    .put(
+        authenticate,
+        authorizeAdmin,
+        updateCategory)
+    .delete(
+        authenticate,
+        authorizeAdmin,
+        DeleteById
+    )
+
+router
+    // Get All categories but different url http://localhost:5000/api/category/categories
+    .route("/categories")
+    .get(listCategory)
+
+router
+    // Get Specific category but different url http://localhost:5000/api/category/:id
+    .route("/:id")
+    .get(
+        authenticate,
+        authorizeAdmin,
+        getCategoryById
+    )
+
+export default router 
