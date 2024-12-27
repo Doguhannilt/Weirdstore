f6619e3b9b3e85798d65a66f0232b7ff2c5dfaf7
doguhannilt <doguhannilt@gmail.com>
2024-12-27 08:41:19 +0300

    upload-service

--- monorepo/upload-service/index.js
+++ monorepo/upload-service/index.js
@@ -3,27 +3,20 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-import connectiontoDB from './src/config/database.js'
-
+import uploadRoutes from './src/routes/uploadRoutes.js'
 
 const app = express()
 
-
-
-connectiontoDB()
+dotenv.config()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use('/api/upload', uploadRoutes)
 
-
-
-
-
-
-
-
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
 
 app.listen(
     process.env.PORT,
-    () => console.log(`Server running on PORT - API GATEWAY: ${process.env.PORT}`)) 
+    () => console.log(`Server running on PORT - UPLOAD SERVICE: ${process.env.PORT}`)) 
