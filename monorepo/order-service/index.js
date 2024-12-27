05d8f05145600f535caf150fd070c430c3fd8116
doguhannilt <doguhannilt@gmail.com>
2024-12-27 08:41:01 +0300

    order-service

--- monorepo/order-service/index.js
+++ monorepo/order-service/index.js
@@ -1,29 +1,19 @@
 // Packages
-import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import connectiontoDB from './src/config/database.js'
-
+import orderRoutes from './src/routes/orderRoutes.js'
 
 const app = express()
-
-
-
+dotenv.config()
 connectiontoDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-
-
-
-
-
-
-
-
+app.use("/api/order", orderRoutes)
 
 app.listen(
     process.env.PORT,
-    () => console.log(`Server running on PORT - API GATEWAY: ${process.env.PORT}`)) 
+    () => console.log(`Server running on PORT - ORDEE SERVICE: ${process.env.PORT}`)) 
